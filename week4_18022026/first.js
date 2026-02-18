aefa04768af2598cf12957bdfeb6c0d2cb380ba3
muuwahh <muktasawale2976@gmail.com>
2026-02-18 06:19:29 +0000

    mm

--- week4_18022026/first.js
+++ week4_18022026/first.js
@@ -1,5 +1,7 @@
+const prompt = require("prompt-sync")();
+
 let secretNumber = Math.floor(Math.random() * 10) + 1;
-let guess = Number(prompt("Guess a number between 1 and 10"));
+let guess = Number(prompt("Guess a number between 1 and 10: "));
 
 if (guess === secretNumber) {
     console.log("Correct! 🎉");
